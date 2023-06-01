5ce0e447d1799f9783a065ccabee0c2f8f57e9e1
jmjeon5533 <jmjeon5533@gmail.com>
2023-06-02 08:17:47 +0900

    WaitSecond

--- Tower_defence/Assets/Script/SpawnManager.cs
+++ Tower_defence/Assets/Script/SpawnManager.cs
@@ -8,15 +8,34 @@ public class SpawnManager : MonoBehaviour
 
     public float SpawnTime;
     private float curTime;
+    public float SpawnStartTime;
+    bool start = false;
+
+    public UnityEngine.UI.Text WaitSecond;
 
     [SerializeField] Vector3 SummonPos;
 
     void Update()
     {
         curTime += Time.deltaTime;
-        if(curTime >= SpawnTime){
-            curTime -= SpawnTime;
-            Instantiate(EnemyList[Random.Range(0,EnemyList.Count)],SummonPos,Quaternion.identity);
+        WaitSecond.enabled = !start;
+        if (start)
+        {
+            if (curTime >= SpawnTime)
+            {
+                curTime -= SpawnTime;
+                Instantiate(EnemyList[Random.Range(0, EnemyList.Count)], SummonPos, Quaternion.identity);
+            }
         }
+        else
+        {
+            WaitSecond.text = $"남은 시간 : {SpawnStartTime -(int)curTime}초";
+            if(curTime >= SpawnStartTime)
+            {
+                start = true;
+                curTime = 0;
+            }
+        }
+
     }
 }
